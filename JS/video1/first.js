f526d76cdfa0690a9cd4156c370e304a783e77a4
Hil@3531 <142313407+HilKalathiya@users.noreply.github.com>
2024-11-20 18:11:08 +0530

    20th nov

---
+++ JS/video1/first.js
@@ -0,0 +1,5 @@
+console.log("first javascript file");
+alert("Jay Swaminarayan Samran")
+var b = prompt("Enter your body count: ");
+var isConform = confirm("Are you sure you are telling me right.");
+console.log("Daim Bro its too low :" + b);
