75773e2da4e449d1cac166c3a617fbcaec8b2234
Hil@3531 <142313407+HilKalathiya@users.noreply.github.com>
2024-11-29 09:58:33 +0530

    29-nov

---
+++ JS/video7/string.js
@@ -0,0 +1,14 @@
+let maharaj = "Jay Swaminarayan";
+
+console.log(maharaj[9]);
+
+console.log(`my maharaj's name is ${maharaj}`);
+
+let a = `my maharaj is ${maharaj}`;
+
+console.log(a);
+let A = a.toUpperCase();
+console.log(A);
+console.log(a.toLowerCase());
+console.log(a.slice(0, 5));
+console.log(a.length);
