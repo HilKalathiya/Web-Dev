633df50406e57d5e0d3ac96f753b0eef90bc717b
Hil@3531 <142313407+HilKalathiya@users.noreply.github.com>
2025-04-11 22:18:34 +0530

    async week lec 2 for 1:40 hr

--- Cohort/week 2/lec-2/promises.js
+++ Cohort/week 2/lec-2/promises.js
@@ -34,5 +34,34 @@ console.log(map.get('name'));
 
 
 // Promise Class
-//it is promise us that i will return you value in future
+//it is promise  us that i will return you value in future
+// A Promis in JS is an object that repersent the eventaual completion(or failure) 
+// of an asychrnous operation and its resulting values.
+function callback1(){
+    console.log("Jay Swaminarayan after 3 sec and returns the promise to run the code after completion of time.")
+}
+setTimeout(callback1,3000) ; // this is also a promises
 
+// how to write real promises
+function setTimeoutPromisified(ms){
+    return new Promise(resolve=>setTimeout(resolve,ms));
+    //retunr object of promis class
+}
+setTimeoutPromisified(3000).then(callback1);
+
+
+// how the promis is gone work
+
+
+function random(resolve) { // resolve is also a function
+    setTimeout(resolve, 5000);
+}
+const p = new Promise(random); // supposed to return u somthing eventually
+console.log(p); // this is a promise object
+
+
+// using the eventual completion of the promis  
+function callback() {
+    console.log("promise resolved after 5 sec");
+}
+p.then(callback); 
